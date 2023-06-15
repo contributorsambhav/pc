4cdc39caa81d96f2383a82ef4d466599e3a4189a
contributorsambhav <Sambhav511974@gmail.com>
2023-06-15 08:00:39 -0700

    nodeserver

--- nodejs/nodeserver.js
+++ nodejs/nodeserver.js
@@ -5,8 +5,8 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end(TMAJ.js);
+  res.setHeader('Content-Type', 'text/plain');
+  res.end("hello world");
 });
 
 server.listen(port, hostname, () => {
