7a7f3cc499a3757291ac25db394520496a82d5ab
contributorsambhav <Sambhav511974@gmail.com>
2023-06-21 12:44:06 +0530

    new commit

---
+++ callbacks.js
@@ -0,0 +1,15 @@
+let a = parseInt(prompt ("Enter a number"))
+let b = parseInt(prompt ("Enter a number"))
+let c = parseInt(prompt ("Enter a number"))
+
+function sum(a,b,c)  {
+return( a+b+c )
+}
+
+let d = parseInt(prompt("ENTER A NUMBER"))
+
+
+function callback(){
+    document.write(sum(a,b,c)**d)
+}
+console.log(callback())
