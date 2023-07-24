be700fcfef6229b26413a97967223081708fa68c
contributorsambhav <Sambhav511974@gmail.com>
2023-07-24 12:18:13 +0530

    connecting schema,model & saving to collection

--- mongoose/index.js
+++ mongoose/index.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/student", {
+
+main().catch(err => console.log(err));
+
+async function main() {
+
+await mongoose.connect("mongodb://localhost/student", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+}
+
 
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
@@ -13,5 +20,22 @@ db.once("open", function () {
 });
 
 const kittySchema = new mongoose.Schema({
-    name: String
-  });
+  name: String,
+});
+
+kittySchema.methods.speak = function speak() {
+  console.log("My name is harrykitty");
+};
+
+const Kitten = mongoose.model("Kitten", kittySchema);
+
+var harrykitty = new Kitten({name : 'harrykitty'})
+
+console.log(harrykitty.name)
+
+const fluffy = new Kitten({ name: 'fluffy' });
+
+fluffy.save();
+  console.log(fluffy.speak)
+
+  harrykitty.save();
