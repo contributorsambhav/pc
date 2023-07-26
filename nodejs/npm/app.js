8ba6ed2097728f2b431bdffbc0419d02caa1bc02
contributorsambhav <Sambhav511974@gmail.com>
2023-07-27 00:12:52 +0530

    static filr serving and pug

--- nodejs/npm/app.js
+++ nodejs/npm/app.js
@@ -1,8 +1,23 @@
 const express = require("express")
-
 const app = express();
-
 const port = 80;
+const path = require('path')
+
+// for serving static files
+app.use("/static",express.static("static"))
+
+
+// set the template engine as pug
+app.set('view engine','pug')
+
+//setting the views dir
+app.set('views',path.join(__dirname,'views'))
+
+//our pug demo endpoint
+app.get("/demo",((req,res)=>{
+    res.status(200).render('demo',{title: " This is pug endpoint ka title",message : "This is pug's demo endpoint"})
+}))
+
 
 app.get("/",((req,res)=>{
     res.send("This is home page of my first Express app get request")
