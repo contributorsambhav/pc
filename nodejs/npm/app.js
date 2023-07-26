b024224eabe77f5f2498b58dfac837fb2c646f73
contributorsambhav <Sambhav511974@gmail.com>
2023-07-27 00:39:37 +0530

    declaring variables in pug file

--- nodejs/npm/app.js
+++ nodejs/npm/app.js
@@ -14,26 +14,39 @@ app.set('view engine','pug')
 app.set('views',path.join(__dirname,'views'))
 
 //our pug demo endpoint
-app.get("/demo",((req,res)=>{
-    res.status(200).render('demo',{title: " This is pug endpoint ka title",message : "This is pug's demo endpoint"})
-}))
+
+// app.get("/demo",((req,res)=>{
+//     res.status(200).render('demo',{title: " This is pug endpoint ka title",message : "This is pug's demo endpoint"})
+// }))
+
+// //GET POST endpoints
+// app.get("/",((req,res)=>{
+//     res.send("This is home page of my first Express app get request")
+// }))
+
+
+// app.post("/",((req,res)=>{
+//     res.send("This is about page of my first Express app post request")
+// }))
+
+// app.get("/about",((req,res)=>{
+//     res.send("This is about page of my first Express app")
+// }))
+
+//This is to declare variables to be used inplain html present in pug file
 
 
 app.get("/",((req,res)=>{
-    res.send("This is home page of my first Express app get request")
+    const con = "This is text content saved as variable"
+    const parameters ={"title": "This is title as object","content":con}
+    res.status(200).render("index.pug",parameters)
 }))
+// Note : parameters is an object that assigns title as given string and content is read from the avriablr con
 
 
-app.post("/",((req,res)=>{
-    res.send("This is about page of my first Express app post request")
-}))
-
-app.get("/about",((req,res)=>{
-    res.send("This is about page of my first Express app")
-}))
-
-
-
+//starting the server
 app.listen(port,(()=>{
     console.log("The application started succesfully on port "+port)
 }))
+
+
