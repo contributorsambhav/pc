c877fa9928f8d0eb747fc8846cd964512ed9fd94
contributorsambhav <Sambhav511974@gmail.com>
2023-08-05 13:08:34 +0530

    Proper comments added

--- Adv_js/destructuring.js
+++ Adv_js/destructuring.js
@@ -1,11 +1,15 @@
+//destructing ,is similar to equality of sets and comparison to get value 
+
 let arr =  [3,5,8,4,5,2,6,5,8,4,7]
-let [a,b,,,...rest] = arr
+let [a,b,,,...rest] = arr//index 2 and 3 are ignored and after 3 are taken into array called rest.
 
 console.log(a)
 console.log(b)
 console.log(rest)
 
-let   {c,d} = {c:1,d:5}
+
+// with curly braces value can be given in different order but syntax is longer as given below.
+let   {c,d} = {d:1,c:5}
 
 console.log(c)
 console.log(d)
