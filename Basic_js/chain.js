6f58076c288f198962a9cd5625c5324d3cff42b0
contributorsambhav <Sambhav511974@gmail.com>
2023-10-31 19:32:38 +0530

    chaining js

--- Basic_js/chain.js
+++ Basic_js/chain.js
@@ -1,9 +1,41 @@
-let delhiweather = new Promise((resolve,reject)=>{
-    setTimeout(() => {
-        console.log("Hi")
-    }, 2000);
-    resolve(69)
-  }).then((value)=>{
-          console.log(value)
-    }
-  )
+let p1 = new Promise((resolve, reject) => {
+  console.log("This is p1");
+  resolve(1);
+}).then((value) => {
+  let p2 = new Promise((resolve, reject) => {
+    myfunc(value);
+    let sec = value * 2;
+    resolve(sec);
+  }).then((value) => {
+    let p3 = new Promise((resolve, reject) => {
+      myfunc(value);
+      let third = value * 3;
+      resolve(third);
+    })
+      .then((value) => {
+        myfunc(value);
+      })
+      .then(() => {
+        myfunc("Undesrstood the way of chaining");
+      });
+  });
+});
+
+p1.then(() => {
+  console.log("Independent p1 call");
+});
+
+// p2.then(()=>{
+//   console.log("Independent p2 call") will give error as p2 is defined in p1 not globally
+// })
+
+// p3.then(()=>{
+//   console.log("Independent p3 call")
+// })
+
+function myfunc(value){
+  setTimeout(() => {
+    
+    console.log(value)
+  }, 3000);
+}
